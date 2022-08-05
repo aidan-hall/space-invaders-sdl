0b9ce50b146905f8b941bb68d30677f20708eb50
Aidan Hall <aidan.hall202@gmail.com>
2022-08-05 18:06:18 +0100

    [tecs] System manager, fixed adding Components.

--- include/tecs.hpp
+++ include/tecs.hpp
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <cassert>
 #include <cstdint>
+#include <tuple>
 #include <typeindex>
 #include <typeinfo>
 #include <unordered_map>
@@ -18,6 +19,44 @@ using ComponentId = std::uint8_t;
 static constexpr std::size_t MAX_COMPONENTS = 64;
 using Signature = std::bitset<MAX_COMPONENTS>;
 
+inline Signature
+componentsSignature(const std::vector<ComponentId> &components) {
+  Signature s;
+  for (auto &c : components) {
+    s.set(c);
+  }
+  return s;
+}
+
+using SystemId = std::size_t;
+struct SystemManager {
+  SystemId nextSystem = 0;
+  std::vector<std::vector<Entity>> systemInterests;
+
+  SystemId registerSystem(const std::vector<Signature> &entitySignatures,
+                          const Signature &sig) {
+    systemInterests.push_back(deriveInterests(entitySignatures, sig));
+
+    return nextSystem++;
+  }
+
+  // Derive which entities a System with the given Signature would be interested
+  // in.
+  static std::vector<Entity>
+  deriveInterests(const std::vector<Signature> &entitySignatures,
+                  const Signature &sig) {
+    std::vector<Entity> interests;
+    for (Entity i = 0; i < entitySignatures.size(); ++i) {
+      if ((entitySignatures[i] & sig) == sig) {
+        interests.push_back(i);
+      }
+    }
+
+    return interests;
+  }
+
+  std::vector<Entity> interestsOf(SystemId id) { return systemInterests[id]; }
+};
 
 struct Coordinator {
 
@@ -27,11 +66,24 @@ struct Coordinator {
 
   ComponentId nextComponentId;
 
+  SystemManager systems;
+  std::vector<bool> systemsUpToDate;
+
   Coordinator() {
     // A Signature is a Component that every Entity implicitly has,
     // identifying what other Components it has.
     nextComponentId = -1;
     registerComponent<Signature>();
+  }
+
+  SystemId registerSystem(const Signature &sig) {
+    auto signatures = getComponents<Signature>();
+
+    return systems.registerSystem(signatures, sig);
+  }
+
+  SystemId registerSystem(const std::vector<ComponentId> &components) {
+    return registerSystem(componentsSignature(components));
   }
 
   inline Entity newEntity() { return nextEntity++; }
@@ -52,23 +104,27 @@ struct Coordinator {
     return componentIds[std::type_index(typeid(Component))];
   }
 
-  template <typename Component>
-  inline void addComponent(Entity e) {
-    auto s = getComponent<Signature>(e);
-    s.set(componentId<Component>());
+  template <typename Component> inline void addComponent(Entity e) {
+    auto &s = getComponent<Signature>(e);
+    auto c = componentId<Component>();
+    s.set(c);
   }
 
-  template <typename Component>
-  inline void removeComponent(Entity e) {
+  template <typename Component> inline void removeComponent(Entity e) {
     auto s = getComponent<Signature>(e);
     s.reset(componentId<Component>());
   }
 
-  template <typename Component>
-  inline Component& getComponent(Entity e) {
+  template <typename Component> inline std::vector<Component> &getComponents() {
+    static std::vector<Component> components;
+    return components;
+  }
+
+  template <typename Component> inline Component &getComponent(Entity e) {
     assert(e < nextEntity);
     assert(hasComponent<Component>(e));
-    static std::vector<Component> components;
+
+    std::vector<Component> &components = getComponents<Component>();
     if (components.size() <= e) {
       components.resize(e + 1);
     }
@@ -76,17 +132,16 @@ struct Coordinator {
     return components[e];
   }
 
-  template <typename Component>
-  inline bool hasComponent(Entity e) {
-    #ifndef NDEBUG
-    if (componentId<Component>() == componentId<Signature>()) {
-      return true;
-    }
-    #endif
+  template <typename Component> inline bool hasComponent(Entity e) {
     return getComponent<Signature>(e).test(componentId<Component>());
   }
 };
 
+// Every Entity implicitly has a Signature.
+template <> inline bool Coordinator::hasComponent<Signature>(Entity e) {
+  std::ignore = e;
+  return true;
+}
 } // namespace Tecs
 
 #endif // TECS_H
--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,18 @@ struct Bar {
   
 };
 
+
 int main() {
   std::cout<< "Hello world!\n";
   Coordinator ecs;
-  const auto FOO_COMPONENT = ecs.registerComponent<Vec2>();
+  SystemManager systems;
+  const auto VEC2_COMPONENT = ecs.registerComponent<Vec2>();
   const auto BAR_COMPONENT = ecs.registerComponent<Bar>();
+  auto physics_system = ecs.registerSystem({VEC2_COMPONENT, BAR_COMPONENT});
   Entity player = ecs.newEntity();
   Entity alien = ecs.newEntity();
-  // std::cout
-  //   << player << ' '
-  //   << alien  << ' '
-  //   << std::endl
-  //   << FOO_COMPONENT << ' ' << ecs.componentId<Vec2>()
-  //   << BAR_COMPONENT << ' ' << ecs.componentId<Bar>()
-  //   << std::endl
-  //   ;
 
+  ecs.addComponent<Vec2>(player);
   auto v = ecs.getComponent<Vec2>(player);
   v.x = 5;
   v.y = 4;
