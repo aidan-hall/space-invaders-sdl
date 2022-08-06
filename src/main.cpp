83404ab743f87d6bb99c254693536ccf74b21af8
Aidan Hall <aidan.hall202@gmail.com>
2022-08-06 11:58:45 +0100

    [main] Deleted contents for fresh start.

--- src/main.cpp
+++ src/main.cpp
@@ -3,42 +3,5 @@
 
 using namespace Tecs;
 
-struct Vec2 {
-  int x;
-  int y;
-};
-struct Bar {
-  
-};
-
-
 int main() {
-  std::cout << "sizeof(Bar) == " << sizeof(Bar) << std::endl;
-  std::cout<< "Hello world!\n";
-  Coordinator ecs;
-  SystemManager systems;
-  const auto VEC2_COMPONENT = ecs.registerComponent<Vec2>();
-  const auto BAR_COMPONENT = ecs.registerComponent<Bar>();
-  auto physics_system = ecs.registerSystem({VEC2_COMPONENT, BAR_COMPONENT});
-  Entity player = ecs.newEntity();
-  std::cout<< "Player 1's ID: " << player << std::endl;
-  Entity alien = ecs.newEntity();
-
-  std::cout << "Interests of Physics system before & after adding Bar to player:\n";
-  ecs.addComponent<Vec2>(player);
-  std::cout << ecs.systems.systemInterests[0].size() << std::endl;
-  ecs.addComponent<Bar>(player);
-  std::cout << ecs.systems.systemInterests[0].size() << std::endl;
-  {
-    auto& v = ecs.getComponent<Vec2>(player);
-    v.x = 5;
-    v.y = 4;
-  }
-
-  ecs.destroyEntity(player);
-
-  auto player2 = ecs.newEntity();
-  std::cout<< "Player 2's ID: " << player2 << std::endl;
-  std::cout << "Interests of physics system now: " << ecs.systems.systemInterests[0].size() << std::endl;
-  
 }
