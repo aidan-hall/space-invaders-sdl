ecbb636e65644534d5636d74a68706acdb8fa981
Aidan Hall <aidan.hall202@gmail.com>
2022-08-06 10:55:43 +0100

    [main] Implemented ID recycling test.

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,19 @@ int main() {
   const auto BAR_COMPONENT = ecs.registerComponent<Bar>();
   auto physics_system = ecs.registerSystem({VEC2_COMPONENT, BAR_COMPONENT});
   Entity player = ecs.newEntity();
+  std::cout<< "Player 1's ID: " << player << std::endl;
   Entity alien = ecs.newEntity();
 
   ecs.addComponent<Vec2>(player);
-  auto v = ecs.getComponent<Vec2>(player);
-  v.x = 5;
-  v.y = 4;
+  {
+    auto& v = ecs.getComponent<Vec2>(player);
+    v.x = 5;
+    v.y = 4;
+  }
+
+  ecs.destroyEntity(player);
+
+  auto player2 = ecs.newEntity();
+  std::cout<< "Player 2's ID: " << player2 << std::endl;
+  
 }
