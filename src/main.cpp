567831d4e855aa2fbd3389eb0bef53319255f314
Aidan Hall <aidan.hall202@gmail.com>
2022-08-06 17:39:52 +0100

    [main] Chasing aliens demo.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SDL_events.h>
 #include <SDL_rect.h>
 #include <SDL_render.h>
+#include <SDL_timer.h>
 #include <SDL_video.h>
 #include <cstdio>
 #include <glm/glm.hpp>
@@ -20,10 +21,17 @@ struct Velocity {
   glm::vec2 v;
 };
 
+struct Player {};
+
 struct RenderCopy {
   SDL_Texture *texture;
   int w;
   int h;
+};
+
+struct Chasing {
+  Entity target;
+  glm::vec1 speed;
 };
 
 // Framerate.
@@ -31,31 +39,49 @@ struct RenderCopy {
 constexpr int32_t SCREEN_FPS = 60;
 constexpr int32_t SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
+void makeStaticSprite(Entity entity, Coordinator &ecs, Position initPos,
+                      SDL_Texture *texture) {
+  ecs.addComponent<Position>(entity);
+  ecs.addComponent<RenderCopy>(entity);
+  ecs.addComponent<Velocity>(entity);
+
+  ecs.getComponent<Position>(entity) = initPos;
+  {
+    auto &rc = ecs.getComponent<RenderCopy>(entity);
+    rc.texture = texture;
+    SDL_QueryTexture(rc.texture, nullptr, nullptr, &rc.w, &rc.h);
+  }
+}
+
 int main() {
   Coordinator ecs;
 
   SDL::Context sdl(SDL_INIT_VIDEO, "Space Invaders",
                    {SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480},
-                   SDL_WINDOW_SHOWN, {"fonts/GroovetasticRegular.ttf"});
+                   SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE,
+                   {"fonts/GroovetasticRegular.ttf"});
 
   printf("SDL initialised\n");
 
-  // Set up player.
   const auto POSITION_COMPONENT = ecs.registerComponent<Position>();
   const auto RENDERCOPY_COMPONENT = ecs.registerComponent<RenderCopy>();
   const auto VELOCITY_COMPONENT = ecs.registerComponent<Velocity>();
+  const auto CHASING_COMPONENT = ecs.registerComponent<Chasing>();
 
+  // Set up player.
   auto player = ecs.newEntity();
-  ecs.addComponent<Position>(player);
-  ecs.addComponent<RenderCopy>(player);
-  ecs.addComponent<Velocity>(player);
+  makeStaticSprite(player, ecs, {{100, 100}},
+                   sdl.loadTexture("art/player.png"));
 
-  ecs.getComponent<Position>(player).p = glm::vec2(200, 200);
-  ecs.getComponent<Velocity>(player).v = glm::vec2(1, 1);
-  {
-    auto &rc = ecs.getComponent<RenderCopy>(player);
-    rc.texture = sdl.loadTexture("art/player.png");
-    SDL_QueryTexture(rc.texture, nullptr, nullptr, &rc.w, &rc.h);
+  // Set up aliens.
+  auto alienTexture = sdl.loadTexture("art/alien1.png");
+  std::vector<Entity> aliens;
+  for (int i = 0; i < 1000; ++i) {
+    auto alien = ecs.newEntity();
+    makeStaticSprite(alien, ecs, {{i * 10, 200 + i * 10}}, alienTexture);
+    ecs.addComponent<Chasing>(alien);
+    ecs.getComponent<Chasing>(alien) = {player, glm::vec1(1)};
+    aliens.push_back(alien);
   }
 
   struct VelocitySystem : public System {
@@ -69,6 +95,22 @@ int main() {
     }
   } velocitySystem(
       componentsSignature({VELOCITY_COMPONENT, POSITION_COMPONENT}), ecs);
+
+  struct ChasingSystem : System {
+    using System::System;
+    void run(const std::set<Entity> &entities, Coordinator &ecs) {
+      for (auto &e : entities) {
+        const auto &chasing = ecs.getComponent<Chasing>(e);
+        const auto &targetPos = ecs.getComponent<Position>(chasing.target).p;
+        const auto &myPos = ecs.getComponent<Position>(e).p;
+        auto &velocity = ecs.getComponent<Velocity>(e);
+
+        velocity = {glm::normalize(targetPos - myPos) * chasing.speed};
+      }
+    }
+  } chasingSystem(componentsSignature({CHASING_COMPONENT, POSITION_COMPONENT,
+                                       VELOCITY_COMPONENT}),
+                  ecs);
 
   // A system that simply calls SDL_RenderCopy().
   struct RenderCopySystem : System {
@@ -109,8 +151,9 @@ int main() {
       }
     }
 
-
+    runSystem(chasingSystem, ecs);
     runSystem(velocitySystem, ecs);
+
     sdl.renderClear();
     runSystem(renderCopySystem, ecs);
     sdl.renderPresent();
