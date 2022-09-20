d7814550ae86c0ca3cbc4317a77ecdd9e8025b15
Aidan Hall <aidan.hall202@gmail.com>
2022-09-20 08:40:08 +0100

    [.dir-locals] Build in build/debug by default.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cmake --build build -j"))))
+((nil . ((compile-command . "cmake --build build/debug -j"))))
